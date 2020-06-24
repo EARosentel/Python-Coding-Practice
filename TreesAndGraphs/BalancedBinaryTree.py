62d86cf656a16d821986020d9416f324bf9eb792
Elisabeth Rosentel <elisabethrosentel@MacBook-Pro.local>
2020-06-24 13:55:42 -0700

    Added binary tree code

--- TreesAndGraphs/BalancedBinaryTree.py
+++ TreesAndGraphs/BalancedBinaryTree.py
@@ -1,10 +1,50 @@
-def balanced_binary_tree():
-    return None
+def recursive_balanced_binary_tree(tree, key=0, depth=0):
+
+    # depth first search
+    if not tree[key]:
+        return depth
+    else:
+        left = recursive_balanced_binary_tree(tree, tree[key][0], depth + 1)
+        print "Left: %s" % left
+
+        if len(tree[key]) == 2:
+            right = recursive_balanced_binary_tree(tree, tree[key][1], depth + 1)
+            print "Right: %s" % right
+        else:
+            right = depth
+
+        if left > right + 1 or right > left + 1 or not right or not left:
+            return False
+        elif depth == 0:
+            return True
+        else:
+            print "Returning depth: %s" % depth
+            return max(left, right)
+
+
+def balanced_binary_tree(tree):
+
+    return True
 
 
 def main():
-    meeting_times = [[1, 10], [7, 8], [2, 4], [2, 5], [1, 4], [7, 9]]
-    print()  #
+    binary_tree = {
+        0: [1, 2],
+        1: [3, 4],
+        2: [5, 6],
+        3: [7, 8],
+        4: [9, 10],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        10: [11],
+        11: []
+    }
+
+    print binary_tree
+    print(balanced_binary_tree(binary_tree))  #
 
 
 if __name__ == '__main__':
